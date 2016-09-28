680a71b2a75d80b7ffa4e274d5a73f6f4f533971
Max <moongust@yandex.ru>
2016-09-28 13:00:35 +0500

    Point with Constructor

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,11 +10,16 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            point p1 = new Point();
-            p1.x = 10;
+            Point p1 = new Point(10,4,'*');
+            Point p2 = new Point(10, 5, '*');
+            
+            /* p1.x = 10;
             p1.y = 4;
             p1.symbol = '*';
+            */
+
             p1.Draw();
+            p2.Draw();
             
             /*Console.SetCursorPosition(x1, y1);
             Console.Write(symbol);
---
+++ Snake/Snake/point.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char symbol;
+
+        public Point()
+        {
+        }
+
+        public Point(int _x, int _y, char _symbol)
+        {
+            x = _x;
+            y = _y;
+            symbol = _symbol;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+        }
+    }
+}
