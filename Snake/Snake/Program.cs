7ccf10203303f991dbfe20d9c3069a488719bfd4
Max <moongust@yandex.ru>
2016-09-28 18:00:59 +0500

    List<int> class

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,16 +12,25 @@ namespace Snake
         {
             Point p1 = new Point(10,4,'*');
             Point p2 = new Point(10, 5, '*');
-            
-            /* p1.x = 10;
-            p1.y = 4;
-            p1.symbol = '*';
-            */
 
             p1.Draw();
             p2.Draw();
+
+            List<int> numlist = new List<int>();
+            numlist.Add(24);
+            numlist.Add(48);
+            numlist.Add(96);
+
+            foreach (int i in numlist)
+            {
+                Console.WriteLine(i);
+            }
             
-            /*Console.SetCursorPosition(x1, y1);
+
+            /* p1.x = 10;
+            p1.y = 4;
+            p1.symbol = '*';
+            Console.SetCursorPosition(x1, y1);
             Console.Write(symbol);
             Console.WriteLine("Hello, world!");
              */ 
